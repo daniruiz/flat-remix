ef7421dae60a1a6342fc68418c73c8e645696102
Daniel Ruiz de Alegría <daniel@drasite.com>
2021-01-21 13:09:11 +0100

    Add custom desktop icons for color-folder script

--- tools/color-folder/generate-colors.sh
+++ tools/color-folder/generate-colors.sh
@@ -7,7 +7,7 @@ color3='#fff'
 colors=(     black     blue      brown     cyan      green     grey      magenta   orange    red       teal      violet    yellow  )
 color1_new=('#383c4a' '#367bf0' '#aa7a50' '#23bac2' '#17917a' '#737680' '#b8174c' '#fd7d00' '#d41919' '#20a8af' '#8c42ab' '#ffd86e')
 color2_new=('#191919' '#2b62c0' '#775e49' '#20a8af' '#12715f' '#4c4c4c' '#93123d' '#b25800' '#b22525' '#198388' '#622e78' '#ffc730')
-color3_new=('#ffffff' '#ffffff' '#ffffff' '#ffffff' '#ffffff' '#ffffff' '#ffffff' '#ffffff' '#ffffff' '#ffffff' '#ffffff' '#000000')
+color3_new=('#fff'    '#fff'    '#fff'    '#fff'    '#fff'    '#fff'    '#fff'    '#fff'    '#fff'    '#fff'    '#fff'    '#000'   )
 
 
 # ******************************
@@ -19,7 +19,7 @@ do
   dir=$TEMPLATES_DIR-$color
   rm -rf $dir
   mkdir $dir
-  cp $TEMPLATES_DIR/* $TEMPLATES_DIR-$color/
+  cp -d $TEMPLATES_DIR/* $TEMPLATES_DIR-$color/
   echo -e " [\e[32m+\e[0m] $color"
   sed -i "s/${color1}/${color1_new[$i]}/g;
           s/${color2}/${color2_new[$i]}/g;
@@ -41,6 +41,7 @@ do
   desktop_icon=$dir/user-$color-desktop.svg
   [ -f $desktop_icon ] \
     && cp -v $desktop_icon $dir/folder-desktop.svg
+  ln -rsvf $dir/folder-desktop.svg $dir/desktop.svg
 
   for file in $folder_files
   do
